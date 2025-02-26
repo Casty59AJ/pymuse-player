dc413eed995b8cf60e96c2b8964a7fc3376db7a6
casty59aj <casty59aj@gmail.com>
2025-02-26 19:52:59 +0400

    updated player.py

---
+++ src/player.py
@@ -0,0 +1,82 @@
+from config import * # import everything from config
+from ui import *
+# from csv_funcs import * # for csv functions
+from mutagen import *
+
+# WIP! I WILL WORK ON THIS LATER!!!
+
+def play_and_pause():
+    pass
+
+def next_song():
+    pass
+
+def prev_song():
+    pass
+
+def stop():
+    pass
+
+def vol_change():
+    pass
+
+def loop_song():
+    pass
+
+def shuffle_song():
+    pass
+
+def remove_selected_song():
+    pass
+
+def remove_all_songs():
+    pass
+
+# ----------------------------------------------------------------------------------------------------------------------#
+                                                # FAVORITES SECTION
+# ----------------------------------------------------------------------------------------------------------------------#
+
+def load_favs_into_app(self):
+    pass
+
+def add_all_songs_to_favs(self):
+    pass
+
+def add_song_to_favs(self):
+    pass
+
+def remove_selected_song_from_favs(self):
+    pass
+
+def remove_all_songs_from_favs(self):
+    pass
+
+# ----------------------------------------------------------------------------------------------------------------------#
+                                                # TIMELINE SECTION
+# ----------------------------------------------------------------------------------------------------------------------#
+
+def time_slider(self):
+    pass
+
+def double_click_song(self):
+    pass
+
+def song_finished(self, status):
+    pass
+
+# ----------------------------------------------------------------------------------------------------------------------#
+                                                # METADATA SECTION
+# ----------------------------------------------------------------------------------------------------------------------#
+
+def read_metadata(self):
+    pass
+
+def load_metadata_photos(self):
+    pass
+
+# ----------------------------------------------------------------------------------------------------------------------#
+                                                # HELPER FUNCS SECTION
+# ----------------------------------------------------------------------------------------------------------------------#
+
+def set_current_song(self):
+    pass
