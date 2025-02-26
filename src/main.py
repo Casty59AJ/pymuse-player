7b747e04b839fdeb85c676bf8d266d2637241617
casty59aj <casty59aj@gmail.com>
2025-02-26 19:24:05 +0400

    updated main.py

---
+++ src/main.py
@@ -0,0 +1,16 @@
+from player import *
+from config import *
+# from csv_funcs import * # for all the CSV-related functions
+
+class Player(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.setupUi(self) # for setting up the UI (it's unfinished for now)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    app.setStyle('Windows')  # Set the application style to 'Windows'
+    window = Player()  # Create an instance of the player window
+    window.show()  # Show the window
+    sys.exit(app.exec())  # Run the application event loop
