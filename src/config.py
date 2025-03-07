25941eadfb0c246e430c8f9bc047c15ff746dfd4
casty59aj <casty59aj@gmail.com>
2025-03-07 12:17:26 +0400

    lol

--- src/config.py
+++ src/config.py
@@ -1,22 +1,22 @@
 # PYMUSE -------------------------- PROTOTYPE VERSION V2
 # NOT FINAL PRODUCT
 
-from PySide6.QtWidgets import *
-from ui import *
-from PySide6.QtCore import *
-from PySide6.QtGui import *
-from PySide6.QtMultimedia import *
+from PySide6.QtWidgets import QWidget, QApplication, QMainWindow, QFileDialog
+from ui import Ui_Win
+from PySide6.QtCore import QUrl, QTime
+from PySide6.QtGui import QIcon
+from PySide6.QtMultimedia import QMediaPlayer, QAudioOutput
 # from csv_funcs import * # for csv shit
 
 # metadata shit
-from mutagen.mp3 import *
-from mutagen.id3 import *
-from mutagen.wave import *
-from mutagen.ogg import *
-from mutagen.flac import *
-from mutagen.m4a import *
-from mutagen.aac import *
-from mutagen.oggvorbis import *
+from mutagen.mp3 import MP3
+from mutagen.id3 import ID3
+from mutagen.wave import WAVE
+from mutagen.ogg import OggFileType
+from mutagen.flac import FLAC
+from mutagen.m4a import M4A
+from mutagen.aac import AAC
+from mutagen.oggvorbis import OggVorbis
 
 # from pop_ups import * #<------- Will implement when coded
 # from cd_play_funcs import * #<------- Will implement when coded
